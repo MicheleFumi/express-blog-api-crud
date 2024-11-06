bdb68b7e3acaee839e09a9f0e039305720d931c4
MicheleFumi <michele.fumi96@gmail.com>
2024-11-06 15:26:20 +0100

    add index and sort by id bonus section

---
+++ BONUS/db/products.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        id: 1,
+        nome: "Laptop",
+        prezzo: 1200,
+        categoria: "Elettronica",
+        disponibile: true
+    },
+    {
+        id: 2,
+        nome: "Smartphone",
+        prezzo: 800,
+        categoria: "Elettronica",
+        disponibile: false
+    },
+    {
+        id: 3,
+        nome: "Tastiera meccanica",
+        prezzo: 100,
+        categoria: "Accessori",
+        disponibile: true
+    },
+    {
+        id: 4,
+        nome: "Sedia da ufficio",
+        prezzo: 250,
+        categoria: "Arredamento",
+        disponibile: true
+    },
+    {
+        id: 5,
+        nome: "Monitor",
+        prezzo: 300,
+        categoria: "Elettronica",
+        disponibile: false
+    }
+];
